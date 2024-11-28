3d4de7c4a0c22ae81967a74a9aaf6864d9c911b5
Pancham Mehta <pancham1305@gmail.com>
2024-11-28 18:06:59 +0530

    Made Contact

---
+++ Contact.java
@@ -0,0 +1,20 @@
+public class Contact {
+    String FirstName;
+    String LastName;
+    String city;
+    String state;
+    String email;
+    int phone;
+    int zip;
+
+    public Contact(String FirstName, String LastName, String city, String state, String email, int phone, int zip) {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.city = city;
+        this.email = email;
+        this.phone = phone;
+        this.state = state;
+        this.zip = zip;
+    }
+    
+}
